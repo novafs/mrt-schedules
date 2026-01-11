58a214b96c57ce0094ddeb929d9b325503990e31
novafs <novafajri20@gmail.com>
2026-01-11 15:39:58 +0700

    Get Station and Schedule Function

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/novafs/mrt-schedules/modules/station"
+)
+
+func main() {
+	InitiateRouter()
+}
+
+func InitiateRouter() {
+	var (
+		router = gin.Default()
+		api    = router.Group("/v1/api")
+	)
+	station.Initiate(api)
+	router.Run(":8080")
+}
